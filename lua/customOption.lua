b1d5814d72891deaf6e31bb032e4fbe9fad9ac44
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-08 16:00:20 +0900

    commit

--- lua/customOption.lua
+++ lua/customOption.lua
@@ -1,3 +1,4 @@
 vim.o.pumheight = 40 -- 補完候補を最大20項目表示する
 vim.o.wrap = true
 vim.opt.clipboard = "unnamedplus"
+
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,3 +3,4 @@ vim.keymap.set('i','<C-h>','<Left>')
 vim.keymap.set('i','<C-j>','<Down>')
 vim.keymap.set('i','<C-k>','<Up>')
 vim.keymap.set('i','<C-l>','<Right>')
+
