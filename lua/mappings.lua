fcef118d203f7bf97dc109478f726adaf35b1609
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-08 19:42:34 +0900

    commit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,18 +3,4 @@ vim.keymap.set('i','<C-h>','<Left>')
 vim.keymap.set('i','<C-j>','<Down>')
 vim.keymap.set('i','<C-k>','<Up>')
 vim.keymap.set('i','<C-l>','<Right>')
-if vim.fn.has('wsl') == 1 then
-  vim.g.clipboard = {
-    name = 'win32yank',
-    copy = {
-      ['+'] = 'win32yank.exe -i --crlf',
-      ['*'] = 'win32yank.exe -i --crlf',
-    },
-    paste = {
-      ['+'] = 'win32yank.exe -o --lf',
-      ['*'] = 'win32yank.exe -o --lf',
-    },
-    cache_enabled = 0,
-  }
-end
 
