7fde149513efc0ce999d5cd1caf490be45bab751
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-09 11:52:30 +0900

    commit

--- lua/customOption.lua
+++ lua/customOption.lua
@@ -2,3 +2,18 @@ vim.o.pumheight = 40 -- 補完候補を最大20項目表示する
 vim.o.wrap = true
 vim.opt.clipboard = "unnamedplus"
 
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+  name = "win32yank",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf"
+    },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf"
+    },
+  cache_enable = 0,
+  }
+end
+
