ff677d8c93bbdde8c8a99ad92bf2ad85e218de97
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-10 11:25:20 +0900

    commit

--- lua/user/option.lua
+++ lua/user/option.lua
@@ -4,6 +4,9 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.clipboard = "unnamedplus"
+-- markdownファイルのみタブ=スペース4個分になる現象をこれで解決できる
+-- ソース: https://www.reddit.com/r/neovim/comments/z2lhyz/when_i_insert_tab_it_expands_to_4_spaces_and_not/?rdt=60892
+vim.g.markdown_recommended_style = 0
 
 if vim.fn.has('wsl') == 1 then
   vim.g.clipboard = {
