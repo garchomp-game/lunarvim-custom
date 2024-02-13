58059f1d29efa8a9522cd25c084fbadc4fcfbba8
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-13 11:59:49 +0900

    commit

--- lua/user/plugins/custom/aerial.lua
+++ lua/user/plugins/custom/aerial.lua
@@ -1,5 +1,7 @@
 return {
-  "stevearc/aerial.nvim",
-  event = { 'CmdLineEnter' },
-  config = true
+	"stevearc/aerial.nvim",
+	event = { "CmdLineEnter" },
+	config = function()
+		vim.api.nvim_set_keymap("n", "<leader>a", "<cmd>AerialToggle!<CR>", { noremap = true, silent = true })
+	end,
 }
