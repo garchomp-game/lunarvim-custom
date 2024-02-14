6a4712bde1f1e57f8140e8051f3280a09a262a53
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-14 10:43:55 +0900

    commit

--- lua/user/plugins/custom/nvim-neo-tree.lua
+++ lua/user/plugins/custom/nvim-neo-tree.lua
@@ -7,19 +7,18 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
+  config = function(_, opts)
     vim.api.nvim_set_keymap(
       'n','<C-n>',
       ':Neotree toggle<CR>',
       { noremap = true, silent = true }
     )
-    require('neo-tree').setup(
-      {
-        window = {
-          width = 25,
-          auto_expand_width = true,
-        }
-      }
-    )
+    require("neo-tree").setup(opts)
   end,
+  opts = {
+    window = {
+      width = 25,
+      auto_expand_width = true,
+    }
+  }
 }
