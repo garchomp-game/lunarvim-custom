1fb58b1aff295119e8d9ffff374c7ddd4d782a43
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-16 11:11:26 +0900

    commit

--- lua/user/plugins/custom/memolist.lua
+++ lua/user/plugins/custom/memolist.lua
@@ -5,7 +5,21 @@ return {
     'preservim/nerdtree'
   },
   config = function()
-    vim.g.memolist_path = vim.env.HOME .. '/memo'
+    local memolist_path = vim.env.HOME .. '/memo'
+    vim.g.memolist_path = memolist_path
     vim.g.memolist_ex_cmd = 'NERDTree'
+
+    -- memoフォルダが存在するか確認
+    if not vim.loop.fs_stat(memolist_path) then
+      -- フォルダが存在しない場合はgit cloneを実行
+      vim.fn.system({
+        "git",
+        "clone",
+        "https://github.com/garchomp-game/memolist",
+        memolist_path,
+        "--branch=main"
+      })
+      print("memolist repository has been cloned to " .. memolist_path)
+    end
   end
 }
