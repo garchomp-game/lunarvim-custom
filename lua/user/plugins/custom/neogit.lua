9c6c4fc1cd54f933255e0bf71e3e614f5e801e8f
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-16 11:19:09 +0900

    commit

--- lua/user/plugins/custom/neogit.lua
+++ lua/user/plugins/custom/neogit.lua
@@ -6,6 +6,15 @@ return {
     "sindrets/diffview.nvim",        -- optional - Diff integration
     "nvim-telescope/telescope.nvim", -- optional
   },
+  init = function()
+    -- `git config --global commit.verbose`の現在の値を取得
+    local verbose_setting = vim.fn.system("git config --global commit.verbose")
+
+    -- `true`に設定されていない場合、それを設定
+    if verbose_setting and verbose_setting:find("true") == nil then
+      vim.fn.system("git config --global commit.verbose true")
+      print("Git commit.verbose has been set to true globally.")
+    end
+  end,
   config = true
 }
-
