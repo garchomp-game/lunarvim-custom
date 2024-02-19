788b121f511c9e35d1276e3a592d9150a139f4f5
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-19 10:12:14 +0900

    commit

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,4 @@
 require("lvim.lsp.manager").setup("marksman")
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({ { command = "markdownlint", filetypes = { "markdown" } } })
+vim.o.filetype = 'lsp_markdown'
