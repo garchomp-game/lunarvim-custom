f5ef28564e1e8ce2d5776ac55f9ea0dd790e7751
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-19 15:34:17 +0900

    commit

--- lua/user/plugins/custom/codeium.lua
+++ lua/user/plugins/custom/codeium.lua
@@ -1,42 +1,13 @@
 return {
-	"Exafunction/codeium.vim",
-	event = "VeryLazy",
-	keys = {
-		"<C-g>",
-		"<c-;>",
-		"<c-,>",
-		"<c-x>",
-		"<c-.>",
-		"<c-y>",
-		"<c-q>",
-		"<c-s>",
-	},
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set("i", "<C-g>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-;>", function()
-			return vim.fn["codeium#CycleCompletions"](1)
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-,>", function()
-			return vim.fn["codeium#CycleCompletions"](-1)
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-x>", function()
-			return vim.fn["codeium#Clear"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-.>", function()
-			return vim.fn["codeium#Complete"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-y>", function()
-			return vim.fn["codeium#YankKey"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-q>", function()
-			return vim.fn["codeium#Dismiss"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<c-s>", function()
-			return vim.fn["codeium#Select"]()
-		end, { expr = true, silent = true })
-	end,
+  "Exafunction/codeium.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+    "onsails/lspkind-nvim"
+  },
+  config = function()
+    require('codeium').setup()
+    table.insert(lvim.builtin.cmp.sources, { name = "codeium" })
+  end
 }
