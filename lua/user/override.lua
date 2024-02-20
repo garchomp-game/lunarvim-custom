8d6803b01e29394f0080f13191a1a169cf04757f
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-20 15:35:23 +0900

    commit

--- lua/user/override.lua
+++ lua/user/override.lua
@@ -7,3 +7,6 @@ lvim.builtin.cmp.cmdline.enable = true
 lvim.builtin.nvimtree.active = false
 -- <Space>fとかでgit grepとかが反応してしまうため
 lvim.builtin.which_key.active = false
+lvim.lsp.installer.setup.ensure_installed = {
+  "stylelint_lsp",
+}
